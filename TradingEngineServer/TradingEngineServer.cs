a55190212b6d426c410119f60a42db6a0a0d5c2d
Milind452 <milind.mohapatra@gmail.com>
2023-10-24 21:47:05 +0530

    make trading engine a background service

--- TradingEngineServer/TradingEngineServer.cs
+++ TradingEngineServer/TradingEngineServer.cs
@@ -1,7 +1,15 @@
+using Microsoft.Extensions.Hosting;
+
 namespace TradingEngineServer.Core
 {
-    internal class TradingEngineServer : ITradingEngineServer
+    internal class TradingEngineServer : BackgroundService, ITradingEngineServer
     {
-
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
