0c7f1da3b7439fd02eb7d948ff099d23780cd945
Milind452 <milind.mohapatra@gmail.com>
2023-10-24 21:50:14 +0530

    inject logger

--- TradingEngineServer/TradingEngineServer.cs
+++ TradingEngineServer/TradingEngineServer.cs
@@ -1,9 +1,15 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace TradingEngineServer.Core
 {
     internal class TradingEngineServer : BackgroundService, ITradingEngineServer
     {
+        private readonly ILogger<TradingEngineServer> _logger;
+        public TradingEngineServer(ILogger<TradingEngineServer> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
