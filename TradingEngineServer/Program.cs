d6c785d12537d87e4841735ea87f9c9a1bdb7c53
Milind452 <milind.mohapatra@gmail.com>
2023-10-27 18:26:18 +0530

    Create a scope and run the engine

--- TradingEngineServer/Program.cs
+++ TradingEngineServer/Program.cs
@@ -1,4 +1,10 @@
-﻿using TradingEngineServer.Core;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TradingEngineServer.Core;
 
 using var engine = TradingEngineServerHostBuilder.BuildTradingEngineServer();
 TradingEngineServerServiceProvider.ServiceProvider = engine.Services;
+{
+    using var scope = TradingEngineServerServiceProvider.ServiceProvider.CreateScope();
+    await engine.RunAsync().ConfigureAwait(false);
+}
