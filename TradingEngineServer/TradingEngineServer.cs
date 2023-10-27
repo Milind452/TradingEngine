fc96af4c48ff8b4e91c994cae57dd15a23102c64
Milind452 <milind.mohapatra@gmail.com>
2023-10-27 17:40:32 +0530

    inject configuration into tradingEngine

--- TradingEngineServer/TradingEngineServer.cs
+++ TradingEngineServer/TradingEngineServer.cs
@@ -1,14 +1,18 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using TradingEngineServer.Core.Configuration;
 
 namespace TradingEngineServer.Core
 {
     internal class TradingEngineServer : BackgroundService, ITradingEngineServer
     {
         private readonly ILogger<TradingEngineServer> _logger;
-        public TradingEngineServer(ILogger<TradingEngineServer> logger)
+        private readonly TradingEngineServerConfiguration _tradingEngineServerConfiguration;
+        public TradingEngineServer(ILogger<TradingEngineServer> logger, IOptions<TradingEngineServerConfiguration> config)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _tradingEngineServerConfiguration = config.Value ?? throw new ArgumentNullException(nameof(config));
         }
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
