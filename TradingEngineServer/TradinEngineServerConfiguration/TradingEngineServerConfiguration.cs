461a50064583a265ced3299cd962b3395e10b99d
Milind452 <milind.mohapatra@gmail.com>
2023-10-27 18:09:32 +0530

    Add config class and settings

--- TradingEngineServer/TradinEngineServerConfiguration/TradingEngineServerConfiguration.cs
+++ TradingEngineServer/TradinEngineServerConfiguration/TradingEngineServerConfiguration.cs
@@ -2,6 +2,11 @@ namespace TradingEngineServer.Core.Configuration
 {
     class TradingEngineServerConfiguration
     {
+        public TradingEngineServerSettings TradingEngineServerSettings { get; set; }
+    }
 
+    class TradingEngineServerSettings
+    {
+        public int Port { get; set; }
     }
 }
