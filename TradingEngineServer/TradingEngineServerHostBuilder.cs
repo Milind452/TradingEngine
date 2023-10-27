462317d8dcb0c4cb0bc25fccfa13cecdec7f00a3
Milind452 <milind.mohapatra@gmail.com>
2023-10-27 18:01:01 +0530

    add host builder for server and add options

---
+++ TradingEngineServer/TradingEngineServerHostBuilder.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TradingEngineServer.Core.Configuration;
+
+namespace TradingEngineServer.Core
+{
+    public sealed class TradingEngineServerHostBuilder
+    {
+        public static IHost BuildTradingEngineServer() =>
+            Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+            {
+                services.AddOptions();
+                services.Configure<TradingEngineServerConfiguration>(
+                    context.Configuration.GetSection(nameof(TradingEngineServerConfiguration))
+                );
+            }).Build();
+    }
+}
